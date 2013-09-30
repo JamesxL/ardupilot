26894c15b62e26a1d93f15c5d64afbe59cad19ad
JamesL <jamesl@precag-HP-Compaq-Elite-8300-CMT.(none)>
2013-09-30 16:24:10 -0500

    add sensor cases for px4.

--- libraries/AP_InertialSensor/AP_InertialSensor_PX4.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_PX4.cpp
@@ -31,6 +31,11 @@ uint16_t AP_InertialSensor_PX4::_init_sensor( Sample_rate sample_rate )
         _default_filter_hz = 30;
         _sample_time_usec = 5000;
         break;
+    case RATE_250HZ:
+    default:
+        _default_filter_hz = 30;
+        _sample_time_usec = 4000;
+        break;
     case RATE_400HZ:
     default:
         _default_filter_hz = 30;
